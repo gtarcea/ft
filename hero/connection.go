82da4e408d7ad3a616fb92ce94f071a441bcdeec
gtarcea <glenn.tarcea@gmail.com>
2020-09-14 16:41:59 -0400

    Message definition moved here

--- hero/connection.go
+++ hero/connection.go
@@ -7,6 +7,12 @@ import (
 	"github.com/apex/log"
 )
 
+type Message struct {
+	Action string `json:"action"`
+	Error  string `json:"error"`
+	Body   []byte `json:"body"`
+}
+
 type connection struct {
 	conn net.Conn
 	ctx  *ctx
@@ -23,6 +29,7 @@ func (c *connection) handleConnection() {
 	for {
 		select {
 		case <-c.ctx.hero.context.Done():
+			_ = c.conn.Close()
 			return
 		default:
 			msg, err := c.readMsg()
@@ -31,6 +38,9 @@ func (c *connection) handleConnection() {
 			}
 			if err := c.runMsgAction(msg); err != nil {
 				log.Debugf("Action returned error: %s", err)
+				if _, err := c.writeError(err); err != nil {
+					log.Debugf("Unable to write error to connection, got error: %s", err)
+				}
 			}
 		}
 	}
@@ -51,4 +61,8 @@ func (c *connection) readMsg() (*Message, error) {
 
 func (c *connection) writeMsg(action string, body interface{}) (int, error) {
 	return WriteMsgToConn(c.conn, action, body, c.ctx.encryptionOn, c.ctx.encryptionKey)
+}
+
+func (c *connection) writeError(err error) (int, error) {
+	return WriteErrorToConn(c.conn, err, c.ctx.encryptionOn, c.ctx.encryptionKey)
 }
