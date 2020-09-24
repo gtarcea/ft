e0a85876d7547515ead19747b3f303d458badb37
gtarcea <glenn.tarcea@gmail.com>
2020-09-24 17:12:06 -0400

    Update documentation

--- pkg/ft/net_packet_conn.go
+++ pkg/ft/net_packet_conn.go
@@ -9,9 +9,11 @@ import (
 
 // The NetPacketConn interface is used to turn ipv4 and ipv6 PacketConn into a single interface since the
 // two implementations are slightly different. The methods below are all methods that are needed on
-// the PacketConn
+// a net.PacketConn that we map to our interface.
 type NetPacketConn interface {
-	Close() error
+	// Close ignores the error on closing the connection (at the point we are closing we don't care if there
+	// is an issue with the network close).
+	Close()
 	JoinGroup(ifi *net.Interface, group net.Addr) error
 	SetMulticastInterface(ini *net.Interface) error
 
@@ -35,13 +37,18 @@ type NetPacketConn4 struct {
 	*ipv4.PacketConn
 }
 
-// ReadFrom wraps the ipv4 ReadFrom without a control message
+// Close wraps the ipv4 Close and ignores the error
+func (p NetPacketConn4) Close() {
+	_ = p.PacketConn.Close()
+}
+
+// ReadFrom wraps the ipv4 ReadFrom and ignores the ControlMessage
 func (p NetPacketConn4) ReadFrom(buf []byte) (int, net.Addr, error) {
 	n, _, addr, err := p.PacketConn.ReadFrom(buf)
 	return n, addr, err
 }
 
-// WriteTo wraps the ipv4 WriteTo without a control message
+// WriteTo wraps the ipv4 WriteTo call and passes nil for the ControlMessage
 func (p NetPacketConn4) WriteTo(buf []byte, dst net.Addr) (int, error) {
 	return p.PacketConn.WriteTo(buf, nil, dst)
 }
@@ -51,18 +58,23 @@ type NetPacketConn6 struct {
 	*ipv6.PacketConn
 }
 
-// ReadFrom wraps the ipv6 ReadFrom without a control message
+// Close wraps the ipv4 Close and ignores the error
+func (p NetPacketConn6) Close() {
+	_ = p.PacketConn.Close()
+}
+
+// ReadFrom wraps the ipv6 ReadFrom and ignores the ControlMessage
 func (p NetPacketConn6) ReadFrom(buf []byte) (int, net.Addr, error) {
 	n, _, addr, err := p.PacketConn.ReadFrom(buf)
 	return n, addr, err
 }
 
-// WriteTo wraps the ipv6 WriteTo without a control message
+// WriteTo wraps the ipv6 WriteTo call and passes nil for the ControlMessage
 func (p NetPacketConn6) WriteTo(buf []byte, dst net.Addr) (int, error) {
 	return p.PacketConn.WriteTo(buf, nil, dst)
 }
 
-// SetMulticastTTL wraps the hop limit of ipv6
+// SetMulticastTTL calls the ipv6 SetMulticastHopLimit instead
 func (p NetPacketConn6) SetMulticastTTL(i int) error {
 	return p.SetMulticastHopLimit(i)
 }
